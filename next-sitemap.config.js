46ad50db3bf964f3246e5d3029595235462aca46
salemadmob-crypto <salemadmob@gmail.com>
2025-10-23 16:22:21 -0700

    Add files via upload

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://www.sanadhome.com',
+  generateRobotsTxt: true, // optional
+  changefreq: 'weekly',
+  priority: 0.7,
+  sitemapSize: 5000,
+};
