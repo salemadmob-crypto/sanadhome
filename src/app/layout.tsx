7a86b9b0d01834741abb570d74be4961080c5c97
salemadmob-crypto <salemadmob@gmail.com>
2025-10-24 10:32:13 -0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,12 +26,24 @@ export default function RootLayout({
   return (
     <html>
       <body className={cairo.className}>
-                
+                <head>
+                <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-89727EPSBV"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
+  gtag('config', 'G-89727EPSBV');
+</script>
+                
+                
+                </head>
         {children}
       </body>
     </html>
   );
 }
+
 
 
